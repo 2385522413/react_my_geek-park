99c398bd45fe94e80f91ecae92c1199d90012cb3
杨楠 <2385522413@qq.com>
2023-02-02 21:36:02 +0800

    navbar的封装

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from './index.module.scss'
+import Icon from "@/components/icon";
+import {useHistory} from "react-router-dom";
+//import {withRouter} from "react-router-dom";
+
+function NavBar({children,extra}) {
+    const history = useHistory();
+    const back=()=>{
+        console.log(history);
+        history.go(-1)
+    }
+    return (
+        <div className={styles.root}>
+            {/* 后退按钮 */}
+            <div className="left" onClick={back}>
+                <Icon type="iconfanhui" />
+            </div>
+            {/* 居中标题 */}
+            <div className="title">{children}</div>
+
+            {/* 右侧内容 */}
+            <div className="right">extra</div>
+        </div>
+    )
+}
+
+//export default withRouter(NavBar);
+export default NavBar;
