4fc76d44093dbfa19bbf1646a2f00f1e25db5ff0
杨楠 <2385522413@qq.com>
2023-02-17 00:32:46 +0800

    Textarea完成

---
+++ src/components/Textarea/index.js
@@ -0,0 +1,29 @@
+import {useState} from "react";
+import classnames from "classnames";
+import styles from "./index.module.scss";
+
+function Textarea({className, value, onChange, placeholder, maxLength = 100}) {
+    const [count, setCount] = useState(value.length || 0);
+
+    const onValueChange = e => {
+        onChange && onChange(e);
+        setCount(e.target.value.length);
+    };
+
+    return (
+        <div className={classnames(styles.root, className)}>
+      <textarea
+          className="textarea"
+          value={value}
+          onChange={onValueChange}
+          maxLength={maxLength}
+          placeholder={placeholder}
+      />
+            <div className="count">
+                {count}/{maxLength}
+            </div>
+        </div>
+    );
+};
+
+export default Textarea;
--- src/pages/Profile/Edit/components/EditInput/index.js
+++ src/pages/Profile/Edit/components/EditInput/index.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./index.module.scss";
 import NavBar from "@/components/NavBar";
+import Textarea from "@/components/Textarea";
 
 function EditInput({onClose,type}) {
+    const [value, setValue] = useState("hahah" || '')
+
+    const onValueChange = e => {
+        setValue(e.target.value)
+    }
     return (
         <div className={style.root}>
             <NavBar extra={<span className="commit-btn">提交</span>}
                     onLeftClick={onClose}>{type==="name"?"编辑昵称":"编辑简介"}</NavBar>
-            <h3>芜湖</h3>
+            <Textarea
+                placeholder="请输入"
+                value={value}
+                onChange={onValueChange}
+            />
         </div>
     );
 }
