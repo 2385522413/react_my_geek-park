7a8967c0884838d017f7c580a7513b88b111927b
杨楠 <2385522413@qq.com>
2023-02-28 23:07:04 +0800

    navbar豫icon改ts环境

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {ReactElement} from "react";
 import styles from "./index.module.scss";
 import Icon from "@/components/Icon";
 import {useHistory} from "react-router-dom";
 import classNames from "classnames";
 
 //import {withRouter} from "react-router-dom";
+type Props = {
+    children: string | ReactElement
+    extra?: string | ReactElement
+    onLeftClick?: () => void
+    className?: string
+}
 
-function NavBar({children, extra, onLeftClick,className}) {
+function NavBar({children, extra, onLeftClick, className}: Props) {
     const history = useHistory();
     const back = () => {
         if (onLeftClick) {
@@ -17,7 +23,7 @@ function NavBar({children, extra, onLeftClick,className}) {
 
     };
     return (
-        <div className={classNames(styles.root,className)}>
+        <div className={classNames(styles.root, className)}>
             {/* 后退按钮 */}
             <div className="left" onClick={back}>
                 <Icon type="iconfanhui"/>
