2acc88ce9223783626f1b2bf7e3b03d2b97f5665
杨楠 <2385522413@qq.com>
2023-03-01 22:04:14 +0800

    authRoute组件改ts环境

--- src/components/AuthRoute/index.tsx
+++ src/components/AuthRoute/index.tsx
@@ -1,10 +1,12 @@
-import {Redirect, Route} from "react-router-dom";
+import { Route, Redirect, RouteProps } from 'react-router-dom'
 import {hasToken} from "@/utils/storage";
 
 // https://reactrouter.com/web/example/auth-workflow
 // https://stackoverflow.com/a/64307442/15443637
-
-const AuthRoute = ({ component: Component, ...rest }) => {
+interface PrivateRouteProps extends RouteProps {
+    component: React.ComponentType<any>
+}
+const AuthRoute = ({ component: Component, ...rest }:PrivateRouteProps) => {
   return (
     <Route
       {...rest}
